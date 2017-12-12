3bb239d08c78f6ef721efc91960c9ddab82c1bef
ELEXILON <elexilon@gmail.com>
2017-12-12 23:36:14 +0100

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  def full_name
+      return profile.full_name if profile?
+
+      email
+    end
+
+    def profile?
+      profile.present? && profile.persisted?
+    end
+
 end
