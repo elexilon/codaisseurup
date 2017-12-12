1666058cd35f83557c0071d6ccdf78a5845b63bd
ELEXILON <elexilon@gmail.com>
2017-12-12 13:26:18 +0100

    One-to-Many: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to :user 
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates_date :starts_at, on_or_before: lambda { ends_at }
+  validates :description, presence: true, length: { maximum: 500 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Event.destroy_all
+User.destroy_all
+
+user1 = User.new(email: "elexilon@elexilon.com")
+user1.password = "123456"
+user2 = User.new(email: "oliver@elexilon.com")
+user2.password = "123456"
+user1.save
+user2.save
+
+users_count = User.count
+puts "#{users_count} users were created."
+
+Event.create!([
+  { name: "Iguanodon", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",user: user1 },
+  { name: "Mosasaurus", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",user: user2 },
+  { name: "Hungarosaurus", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",user: user1 },
+])
+
+event_count = Event.count
+puts "#{event_count} events were created."
