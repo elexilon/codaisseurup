6ebba7f3e26e612b7c7fbacda5e054bd1fe69c2b
ELEXILON <elexilon@gmail.com>
2017-12-12 17:42:43 +0100

    Add Room Views

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,15 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :starts_at, presence: true
   validates :ends_at, presence: true
-  validates_date :starts_at, on_or_before: lambda { ends_at }
+  #validates_date :starts_at, on_or_before: lambda { ends_at }
   validates :description, presence: true, length: { maximum: 500 }
+
+  validate :start_must_be_before_end_time
+
+  private
+
+  def start_must_be_before_end_time
+      errors.add(:starts_at, "Must be before end time") unless
+          starts_at < ends_at
+  end
 end
---
+++ db/migrate/20171212130332_add_default_to_events.rb
@@ -0,0 +1,8 @@
+class AddDefaultToEvents < ActiveRecord::Migration[5.1]
+  def change
+    change_column :events, :price, :decimal, :default => 0
+    change_column :events, :includes_food, :boolean, :default => false
+    change_column :events, :includes_drinks, :boolean, :default => false
+    change_column :events, :active, :boolean, :default => true
+  end
+end
