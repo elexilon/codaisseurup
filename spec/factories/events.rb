0ecca0d3000f86dbe20ca1846ed97f08a8abac8d
ELEXILON <elexilon@gmail.com>
2017-12-13 11:35:48 +0100

    Add navbar specs

---
+++ spec/factories/events.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :event do
+    name                { Faker::Lorem.words(3).join(' ') }
+    description         { Faker::Lorem.sentence(40) }
+    location            { Faker::Address.city }
+    price               10
+    capacity            100
+    includes_food       true
+    includes_drinks     true
+    starts_at           DateTime.now
+    ends_at             DateTime.now.next_day(1)
+    user                { build(:user) }
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
